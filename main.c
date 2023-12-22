1cbdbb92a907345187f48b620d7551e1c1253f3d
TTIIZZ <wjdghwls0514@gmail.com>
2023-12-22 18:24:06 +0900

    block 내부의 변수를 다 나눈 후, 태그 계산 후 출력까지 확인

--- main.c
+++ main.c
@@ -9,17 +9,21 @@
 // 4-way associative cache이며, block size는 4B이고 총 4개의 set으로 구성됨.
 // 총 캐시 사이즈는 4B*4*4 = 64B.
 
-// 메인 메모리의 크기는 256B. (8-bit address -> 2^8 = 256)
+// 메인 메모리의 크기는 256B. (8-bit address -> (2^8)B = 256B)
 // **************************************************************************
 
 // cache block
 // valid, dirty: 기존 의미와 같음.
-// count: LRU를 위한 bit. 2bits 할당.
+// lru: LRU를 위한 bit. 2bits 할당.
 // tag: 8-2-2 = 4. (offset bit 2개, index bit 2개 빼기)
 typedef struct CacheBlock
 {
-	// valid:1 / dirty:1 / count:2 / tag:4 / data:32 -> 총합 40.
-	unsigned long long data : 40;
+	// valid:1 / dirty:1 / lru:2 / tag:4 / data:32 -> 총합 40.
+	unsigned char valid : 1;
+	unsigned char dirty : 1;
+	unsigned char lru : 2;
+	unsigned char tag : 4;
+	unsigned long data : 32;
 } CacheBlock;
 typedef CacheBlock* CacheSet;
 typedef CacheSet* CacheMemory;
@@ -34,19 +38,21 @@ typedef unsigned char* MainMemory;
 CacheMemory cache_memory;
 MainMemory main_memory;
 
-void CacheAccess();
-unsigned char GetHash();
+void CacheAccess(CacheMemory cache_memory, Address address, int size);
+unsigned char GetIndex(Address address);
 
 int main(void)
 {
+	// 캐시 메모리와 메인 메모리 할당
 	cache_memory = (CacheMemory)calloc(4, sizeof(CacheSet));
 	for (int i = 0; i < 4; i++) cache_memory[i] = (CacheSet)calloc(4, sizeof(CacheBlock));
 	main_memory = (MainMemory)calloc(256, sizeof(Byte));
 	for (int i = 0x0; i <= 0xff; i++) main_memory[i] = i;  // address와 data를 같은 2진수로 초기화
 
-	cache_memory[0][0].data = cache_memory[0][0].data | 0x0f0f0f0f0f;
-	CacheAccess(cache_memory, 0x11);	
+	cache_memory[0][0].data = 0xf0f0f0f0;
+	CacheAccess(cache_memory, 0x11, 4);
 
+	// 캐시 메모리와 메인 메모리 해제
 	for (int i = 0; i < 4; i++) free(cache_memory[i]);
 	free(cache_memory);
 	free(main_memory);
